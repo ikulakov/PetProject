f536cb23ff8a1fe3c4d03cdb0e178a0d5d4b236e
ikulakov <ivan.kulakov@mail.ru>
2023-02-10 20:22:34 +0300

    add jest first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,30 @@
+import { classNames } from './classNames'
+
+describe('classNames', () => {
+    test('with first param', () => {
+        expect(classNames('someClass')).toBe('someClass')
+    })
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2'
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected)
+    })
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2 hovered'
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrolable: false },
+            ['class1', 'class2']
+        )).toBe(expected)
+    })
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2 hovered'
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrolable: undefined },
+            ['class1', 'class2']
+        )).toBe(expected)
+    })
+})
