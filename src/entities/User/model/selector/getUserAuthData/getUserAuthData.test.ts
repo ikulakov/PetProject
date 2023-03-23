b8cf6a0164b48589b44b5af100dee77ca54f5570
ikulakov <ivan.kulakov@mail.ru>
2023-03-23 14:05:50 +0300

    -

---
+++ src/entities/User/model/selector/getUserAuthData/getUserAuthData.test.ts
@@ -0,0 +1,22 @@
+import { StateSchema } from 'app/providers/StoreProvider'
+import { getUserAuthData } from './getUserAuthData'
+
+describe('getProfileData.test', () => {
+    test('should return data', () => {
+        const data = {
+            id: '1',
+            username: 'admin',
+            avatar: 'link_to_avatar'
+        }
+        const state: DeepPartial<StateSchema> = {
+            user: {
+                authData: data
+            }
+        }
+        expect(getUserAuthData(state as StateSchema)).toEqual(data)
+    })
+    test('shoul work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {}
+        expect(getUserAuthData(state as StateSchema)).toEqual(undefined)
+    })
+})
