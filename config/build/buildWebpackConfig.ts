c9742c3f38c49f7afb0720de422027fccd94f41b
ikulakov <ivan.kulakov@mail.ru>
2023-05-03 13:34:09 +0300

    update webpack config

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -30,7 +30,7 @@ export function buildWebpackConfig (options: BuildOptions): webpack.Configuratio
             } //
         },
         resolve: buildResolvers(options),
-        devtool: isDev ? 'inline-source-map' : undefined,
+        devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
         devServer: isDev ? buildDevServer(options) : undefined
     };
 }
--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -1,8 +1,9 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export function buildCssLoader (isDev: boolean) {
     return {
         test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
         use: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
@@ -18,5 +19,5 @@ export function buildCssLoader (isDev: boolean) {
             },
             'sass-loader'
         ]
-    }
+    };
 }
