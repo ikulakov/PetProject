667c72cba4353106563c8a1b0ae2cf96988b6c67
ikulakov <ivan.kulakov@mail.ru>
2023-05-06 18:07:22 +0300

    add script for deploy, example nginx.conf

--- json-server/index.js
+++ json-server/index.js
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const fs = require('fs')
-const jsonServer = require('json-server')
+const http = require('http')
+const https = require('https')
 const path = require('path')
+const jsonServer = require('json-server')
+
+const options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+}
 
 const server = jsonServer.create()
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'))
@@ -25,11 +33,13 @@ server.use(async (req, res, next) => {
 server.post('/login', (req, res) => {
     try {
         const { username, password } = req.body
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'))
+        const db = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'),
+        )
         const { users = [] } = db
 
         const userFromBd = users.find(
-            (user) => user.username === username && user.password === password
+            (user) => user.username === username && user.password === password,
         )
 
         if (userFromBd) {
@@ -48,12 +58,23 @@ server.use((req, res, next) => {
     if (!req.headers.authorization) {
         return res.status(403).json({ message: 'AUTH ERROR' })
     }
+
     next()
 })
 
 server.use(router)
 
 // запуск сервера
-server.listen(8000, () => {
-    console.log('server is running on 8000 port')
-})
+const PORT = 8443;
+const HTTP_PORT = 8000;
+
+const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
+httpsServer.listen(PORT, () => {
+    console.log(`server is running on ${PORT} port`);
+});
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`server is running on ${HTTP_PORT} port`);
+});
